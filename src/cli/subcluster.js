0a8b6af46f99954bb9a480c34d7d80eb8371eda7
Steven Weaver <sweaver@temple.edu>
2019-02-20 11:26:23 -0500

    moving main subcluster source

--- src/cli/subcluster.js
+++ src/cli/subcluster.js
@@ -1,13 +1,20 @@
-var subcluster = require("../subcluster");
-
-var commander = require("commander");
 var fs = require("fs");
+var commander = require("commander");
+var winston = require("winston");
 
+var subcluster = require("../subcluster");
 var _networkCDCDateField = "hiv_aids_dx_dt";
-
 var today = new Date();
 
-fs.readFile("./DummyNetwork.json", (err, data) => {
+commander
+  .option("-i --input <input>", "Input HIV-TRACE results file")
+  .option(
+    "-o --output <output>",
+    "Output HIV-TRACE results file with subclusters"
+  )
+  .parse(process.argv);
+
+fs.readFile(commander.input, (err, data) => {
   let shiv_results = JSON.parse(data);
 
   let new_json = subcluster.annotate_priority_clusters(
@@ -20,4 +27,9 @@ fs.readFile("./DummyNetwork.json", (err, data) => {
   );
 
   // Write out new json with subclusters
+  fs.writeFile(commander.output, JSON.stringify(new_json), d => {
+    winston.info(
+      "Subcluster inference completed. Please proceed with further downstream analysis."
+    );
+  });
 });
